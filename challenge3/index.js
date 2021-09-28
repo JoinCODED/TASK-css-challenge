a3794334d8313195a49bd17515f894e70fa33710
omsi96 <omsi96@gmail.com>
2021-09-28 14:08:37 +0300

    adding index.js for challenge 3

---
+++ challenge3/index.js
@@ -0,0 +1,19 @@
+function deleteItem(key) {
+  alert(`You are trying to delete item of key ${key}`);
+}
+
+/** Don't touch this function
+ *  This is a component that will be rendered inside the list
+ */
+const listComponent = (person) => `
+    <div onclick="deleteItem(${person.key})" class="row"> ${person.name} | ${person.address}</div>
+`;
+/**
+ * This function will render the list of id "rows" with the array defined in people.js
+ */
+function renderList() {
+  document.getElementById("rows").innerHTML = people
+    .map(listComponent)
+    .join("\n");
+}
+renderList();
---
+++ challenge3/people.js
@@ -0,0 +1,81 @@
+/**
+ * Don't touch this file
+ */
+
+let people = [
+  {
+    key: "1",
+    name: "Omar Hasan",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "2",
+    name: "Hasan Ali",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "3",
+    name: "Ahmad Mohammad",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "4",
+    name: "Shaima Omar",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "5",
+    name: "Moudhi Khalid",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "6",
+    name: "Laila Aziz",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "7",
+    name: "Salwa Mshary",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "8",
+    name: "Hashim Ahmad",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "9",
+    name: "Ahmad Hussain",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "10",
+    name: "Aziz Khalid",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "11",
+    name: "Noor Kazem",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "12",
+    name: "Nasser Jaber",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "13",
+    name: "Zainab Kamel",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "14",
+    name: "Mohammad Qasem",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+  {
+    key: "15",
+    name: "Jaafar Ali",
+    address: "Kuwait - Hawalli, Salwa, B1, S9, H25",
+  },
+];
